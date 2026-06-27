c20da49fb7b3c1ef4050106b9d5c0100fadc0440
maven1humbled <pedrosouzacortezia@gmail.com>
2026-06-27 00:20:15 -0300

    roleta de açoes, feedback visual 1/4, point clicker e 4 ordens

--- JogoPirata_Unity/Assets/Scripts/SelectionManager.cs
+++ JogoPirata_Unity/Assets/Scripts/SelectionManager.cs
@@ -6,7 +6,6 @@ public class SelectionManager : MonoBehaviour
 
     private PirateController pirataSelecionado;
 
-
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -14,165 +13,109 @@ public class SelectionManager : MonoBehaviour
             VerificarClique();
         }
 
-
         if (Input.GetMouseButtonDown(1))
         {
             CancelarSelecao();
         }
     }
 
-
-
     void VerificarClique()
     {
-        Vector2 mousePos =
-        Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-
-        RaycastHit2D hit =
-        Physics2D.Raycast(mousePos, Vector2.zero);
-
-
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
         if (hit.collider == null)
-        {
             return;
-        }
-
-
 
         Debug.Log("Clicou em: " + hit.collider.name);
 
-
-
-        // Primeiro verifica se clicou em um pirata
-
-        PirateController pirata =
-        hit.collider.GetComponent<PirateController>();
-
+        // Verifica se clicou em um pirata
+        PirateController pirata = hit.collider.GetComponent<PirateController>();
 
         if (pirata != null)
         {
-
             if (pirataSelecionado != null)
             {
                 pirataSelecionado.Deselecionar();
             }
 
-
             pirataSelecionado = pirata;
-
 
             pirataSelecionado.Selecionar();
 
-
-            // abre a roleta em cima do pirata
-
-            menuAcoes.Abrir(
-            pirata.transform.position
-            );
-
+            menuAcoes.Abrir(pirata.transform.position);
 
             return;
         }
 
-
-
-        // Se futuramente clicar em objetos do mapa
-        // fica preparado aqui
-
-
-        ObjetoInterativo objeto =
-        hit.collider.GetComponent<ObjetoInterativo>();
-
+        // Futuramente servirá para clicar em objetos do cenário
+        ObjetoInterativo objeto = hit.collider.GetComponent<ObjetoInterativo>();
 
         if (objeto != null && pirataSelecionado != null)
         {
             objeto.ReceberOrdem(pirataSelecionado);
 
-            menuAcoes.Fechar();
+            FinalizarSelecao();
         }
-
     }
-
-
-
-
 
     void CancelarSelecao()
     {
-
-        if (pirataSelecionado != null)
-        {
-            pirataSelecionado.Deselecionar();
-
-            pirataSelecionado = null;
-        }
-
-
-        menuAcoes.Fechar();
-
+        FinalizarSelecao();
 
         Debug.Log("Seleção cancelada");
     }
 
+    // Fecha o menu e remove a seleção do pirata
+    void FinalizarSelecao()
+    {
+        if (pirataSelecionado != null)
+        {
+            pirataSelecionado.Deselecionar();
+            pirataSelecionado = null;
+        }
 
-
-
+        menuAcoes.Fechar();
+    }
 
     public void OrdemCanhao()
     {
         if (pirataSelecionado == null)
             return;
 
-
         pirataSelecionado.ReceberOrdem(Ordem.Canhao);
 
-        menuAcoes.Fechar();
+        FinalizarSelecao();
     }
-
-
-
-
 
     public void OrdemIncendio()
     {
         if (pirataSelecionado == null)
             return;
 
-
         pirataSelecionado.ReceberOrdem(Ordem.Incendio);
 
-        menuAcoes.Fechar();
+        FinalizarSelecao();
     }
-
-
-
-
 
     public void OrdemAgua()
     {
         if (pirataSelecionado == null)
             return;
 
-
         pirataSelecionado.ReceberOrdem(Ordem.Agua);
 
-        menuAcoes.Fechar();
+        FinalizarSelecao();
     }
-
-
-
-
 
     public void OrdemBuraco()
     {
         if (pirataSelecionado == null)
             return;
 
-
         pirataSelecionado.ReceberOrdem(Ordem.Buraco);
 
-        menuAcoes.Fechar();
+        FinalizarSelecao();
     }
 }
